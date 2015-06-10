ab4aea1105a0401e3c10e37f173f22bc69cf0de9
Stefan Kerstan <stefan.kerstan@googlemail.com>
2015-06-10 09:48:44 +0200

    Changed from "amixer" to "ponymix" to save space because amixer need to install gnome, too.

--- sxhkd/actions/volmute.sh
+++ sxhkd/actions/volmute.sh
@@ -2,14 +2,17 @@
 #!/bin/bash
 #mute and unmute speakers via software
 #get volume
-VOLUME="$(amixer cset numid=3,iface=MIXER,name='Master Playback Volume' | grep ': values=' | cut -f 2 -d ',')"
+#VOLUME="$(amixer cset numid=3,iface=MIXER,name='Master Playback Volume' | grep ': values=' | cut -f 2 -d ',')"
 
 #set stae of mic
-if [ "$VOLUME" == "0" ]; then
+#if [ "$VOLUME" == "0" ]; then
        	#set volume to half of max volume to enable ist again       
-        amixer cset numid=3,iface=MIXER,name='Master Playback Volume' 32500
-else
-        amixer cset numid=3,iface=MIXER,name='Master Playback Volume' off
-fi
+#        amixer cset numid=3,iface=MIXER,name='Master Playback Volume' 32500
+#else
+#        amixer cset numid=3,iface=MIXER,name='Master Playback Volume' off
+#fi
 
+#amixer is realted to the gnome desktop enviroment. In order so save space "ponymix" will be used to control the volume
+# Toggle mute/unmute
+ponymix -d 0 toggle
 

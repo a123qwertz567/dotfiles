86408eb994e53f0022eb56de9d286404df598436
Stefan Kerstan <stefan.kerstan@googlemail.com>
2015-06-10 10:15:53 +0200

    Changed from "amixer" to "ponymix"

--- sxhkd/actions/voldw.sh
+++ sxhkd/actions/voldw.sh
@@ -3,18 +3,24 @@ DISPLAY=:0
 
 #script das mit amixer die lautstaerke einstellt
 #max lautstaerke ermitteln
-VOLMAX="$(amixer cset numid=3,iface=MIXER,name='Master Playback Volume' | grep 'max' | cut -f 6 -d '=' | cut -f 1 -d ',')"
+#VOLMAX="$(amixer cset numid=3,iface=MIXER,name='Master Playback Volume' | grep 'max' | cut -f 6 -d '=' | cut -f 1 -d ',')"
 
 #5 prozent inkrement setzen
-VOLINC=3276
+#VOLINC=3276
 
 #die aktuelle volume holen
-VOLUME="$(amixer cset numid=3,iface=MIXER,name='Master Playback Volume' | grep ': values=' | cut -f 2 -d ',')"
+#VOLUME="$(amixer cset numid=3,iface=MIXER,name='Master Playback Volume' | grep ': values=' | cut -f 2 -d ',')"
 
 #um den 5 Prozent wert senken
 #addieren von werten
-VOLNEW=$(($VOLUME - $VOLINC))
+#VOLNEW=$(($VOLUME - $VOLINC))
 
 #lautstaerke setzen
-amixer cset numid=3,iface=MIXER,name='Master Playback Volume' $VOLNEW
+#amixer cset numid=3,iface=MIXER,name='Master Playback Volume' $VOLNEW
+
+#Changed from "amixer" to "ponymix"
+#Volumestats "ponymix 6" results in "xfce4-volume 1%"
+# 8 points in ponymix results in 10%
+
+ponymix -d 0 decrease 3
 
--- sxhkd/actions/volup.sh
+++ sxhkd/actions/volup.sh
@@ -3,17 +3,21 @@ DISPLAY=:0
 
 #script das mit amixer die lautstaerke einstellt
 #max lautstaerke ermitteln
-VOLMAX="$(amixer cset numid=3,iface=MIXER,name='Master Playback Volume' | grep 'max' | cut -f 6 -d '=' | cut -f 1 -d ',')"
+#VOLMAX="$(amixer cset numid=3,iface=MIXER,name='Master Playback Volume' | grep 'max' | cut -f 6 -d '=' | cut -f 1 -d ',')"
 
 #5 prozent inkrement setzen
-VOLINC=3276
+#VOLINC=3276
 
 #die aktuelle volume holen
-VOLUME="$(amixer cset numid=3,iface=MIXER,name='Master Playback Volume' | grep ': values=' | cut -f 2 -d ',')"
+#VOLUME="$(amixer cset numid=3,iface=MIXER,name='Master Playback Volume' | grep ': values=' | cut -f 2 -d ',')"
 
 #um den 5 Prozent wert erhoehen
 #addieren von werten
-VOLNEW=$(($VOLINC + $VOLUME))
+#VOLNEW=$(($VOLINC + $VOLUME))
 
 #lautstaerke setzen
-amixer cset numid=3,iface=MIXER,name='Master Playback Volume' $VOLNEW
+#amixer cset numid=3,iface=MIXER,name='Master Playback Volume' $VOLNEW
+
+#Changed from amixer to ponymix
+
+ponymix --notify -d 0 increase 3
